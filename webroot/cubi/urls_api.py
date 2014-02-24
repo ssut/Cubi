7f1b236e0a82c1d37357723eb8d45c7b0b8015b6
Lee HanYeong <Arcanelux@gmail.com>
2014-02-25 02:06:42 +0900

    Chapter API추가

--- webroot/cubi/urls_api.py
+++ webroot/cubi/urls_api.py
@@ -14,6 +14,8 @@ urlpatterns += patterns('work.api',
     url(r'^chapter/view/$', 'chapter_view'),
     url(r'^chapter/comment/list/$', 'chapter_comment_list'),
     url(r'^chapter/comment/add/$', 'chapter_comment_add'),
+    url(r'^chapter/rating/$', 'chapter_rating'),
+    url(r'^chapter/rating/add/$', 'chapter_rating_add'),
 )
 
 
--- webroot/work/api.py
+++ webroot/work/api.py
@@ -18,6 +18,22 @@ from cubi.functions import return_failed_json, return_success_json
 from work.models import *
 
 
+
+'''
+Work
+'''
+
+
+'''
+Chapter
+    chapter_list : 댓글 목록
+    chapter_view : 해당 chapter의 webview화면
+    chapter_comment_list : 댓글 목록
+    chapter_commnet_add : 댓글 추가
+    chapter_comment_del : 댓글 삭제
+    chapter_rating : 평점
+    chapter_rating_add : 평점 추가
+'''
 @csrf_exempt
 def chapter_list(request):
     if request.method == 'POST':
@@ -44,7 +60,6 @@ def chapter_list(request):
 
         data = {
             'work': work.json(),
-            # 'chapters': [chapter.json() for chapter in chapters],
             'chapters': chapter_info_list,
         }
 
@@ -73,9 +88,6 @@ def chapter_view(request):
     else:
         return return_failed_json('Must POST Request')
 
-'''
-Chapter - 댓글(Comment), 평점(Rating)
-'''
 # 댓글 목록
 @csrf_exempt
 def chapter_comment_list(request):
@@ -154,3 +166,57 @@ def chapter_comment_del(request):
     else:
         return return_failed_json('Must POST Request')
 
+@csrf_exempt
+def chapter_rating(request):
+    if request.method == 'POST':
+        query_dict = request.POST
+        work_id = int(query_dict['work_id'])
+        chapter_id = int(query_dict['chapter_id'])
+
+        chapter = Chapter.objects.get(id=chapter_id)
+        dict = {}
+
+        # 평점 분석
+        ratings = ChapterRating.objects.filter(chapter=chapter)
+        avg_rating = ratings.aggregate(Avg('score'))['score__avg']
+        if avg_rating:
+            dict['rating'] = avg_rating
+            dict['rating_number'] = ratings.count()
+        else:
+            dict['rating'] = 0.0
+            dict['rating_number'] = 0
+
+        return HttpResponse(json.dumps(dict), content_type='application/json')
+    else:
+        return return_failed_json('Must POST Request')
+
+@csrf_exempt
+def chapter_rating_add(request):
+    if request.method == 'POST':
+        query_dict = request.POST
+        session_key = query_dict['session_key']
+        username = query_dict['username']
+        work_id = int(query_dict['work_id'])
+        chapter_id = int(query_dict['chapter_id'])
+        rating = int(query_dict['rating'])
+
+        s = SessionStore(session_key=session_key)
+
+        if username == s['username']:
+            user = User.objects.get(username=s['username'])
+            work = Work.objects.get(id=work_id)
+            chapter = Chapter.objects.get(work=work, id=chapter_id)
+
+            if ChapterRating.objects.filter(chapter=chapter).filter(author=user).exists():
+                rating_instance = ChapterRating.objects.get(chapter=chapter, author=user)
+                rating_instance.score = rating
+                rating_instance.save()
+            else:
+                rating_instance = ChapterRating.objects.create(chapter=chapter, author=user, score=rating)
+                rating_instance.save()
+            return return_success_json()
+        else:
+            return return_failed_json('Not Matching User')
+
+    else:
+        return return_failed_json('Must POST Request')
