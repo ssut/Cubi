098559aa3f3b6e3b13df54689f163686e9f86a4a
ssut <ssut@ssut.me>
2014-03-29 00:00:27 +0900

    1시간마다 크롤링하는 celery task 생성

--- webroot/work/tasks.py
+++ webroot/work/tasks.py
@@ -3,18 +3,22 @@ from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from datetime import datetime, timedelta
 
+from work import crawl as crawler
+
 from .models import *
 
 # 매 분마다 실행되는 작업
 @periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))
 def crawl_webtoon():
-    # 큐에서 처리할 작업을 하나 가져온다
     queue = ChapterQueue.filter(is_checked=False).order_by('created_at')[0]
 
-    crawler = None
-    if queue.target == u'네이버':
-        return
 
-    # TODO.
-    # 크롤 함수 완성 후에 계속 개발 진행
-    # 여러 인수를 받는 옵션이 필요함
+# 1시간마다 실행되는 작업 (연속적으로 크롤링해야 할 때)
+@periodic_task(run_every=crontab(hour="*", minute="0", day_of_week="*"))
+def crawl_periodic():
+    date = datetime.now()
+    date.replace(minute=0, second=0, microsecond=0)
+    list = ChapterPeriodicQueue.filter(last_run_at__lte=date)
+    for item in list:
+        crawler.crawl(type=item.target, comic_number=item.comic_number, user=item.user)
+        item.last_run_at = datetime.now()
