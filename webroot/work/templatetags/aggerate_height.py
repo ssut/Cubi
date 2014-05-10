3c19101511030dfe90f6b2f96ab6c31f8ec5caa7
ssut <ssut@ssut.me>
2014-05-10 19:53:34 +0900

    웹툰 그리는 부분 수정
    draw with html5 canvas

---
+++ webroot/work/templatetags/aggerate_height.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+from django.template import Library
+register = Library()
+
+@register.filter
+def aggerate_height(list):
+    return sum(image.image.height for image in list)
+
+@register.filter
+def calc_offset(self, index):
+    offset = 0
+    for i in range(1, index):
+        offset += self[i - 1].image.height
+    return offset
