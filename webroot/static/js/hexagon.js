f3f17080f2cbae82735fb53d40207095b5bdca41
ssut <ssut@ssut.me>
2014-06-02 15:22:44 +0900

    작품 목록 페이지 템플릿 적용

--- webroot/static/js/hexagon.js
+++ webroot/static/js/hexagon.js
@@ -51,9 +51,80 @@
     return hex.ctx.invoke(name)(args);
   };
 
+  hex.env = (function() {
+    var UA, Version, isAndroid, isApple, isChrome, isExplorer, isFirefox, isMacintosh, isMobile, isOpera, isSafari, isWebkit, isWindows, uaCheck;
+    uaCheck = function(regularExpression) {
+      return (new RegExp(regularExpression)).test(navigator.userAgent);
+    };
+    Version = 0;
+    UA = navigator.userAgent;
+    isSafari = uaCheck('WebKit') && uaCheck('Safari') && !uaCheck('Chrome') && !uaCheck('OPR');
+    isChrome = uaCheck('WebKit') && uaCheck('Safari') && uaCheck('Chrome') && !uaCheck('OPR');
+    isFirefox = uaCheck('Firefox');
+    isExplorer = uaCheck('MSIE');
+    isOpera = uaCheck('Opera') && !uaCheck('WebKit') || uaCheck('WebKit') && uaCheck('Safari') && uaCheck('Chrome') && uaCheck('OPR');
+    isWebkit = uaCheck('WebKit');
+    isMobile = uaCheck('Mobile');
+    isAndroid = uaCheck('Android');
+    isApple = uaCheck('iP(hone|ad|od)');
+    isWindows = uaCheck('Windows');
+    isMacintosh = uaCheck('Macintosh');
+    if (isSafari || isOpera && !isWebkit) {
+      Version = (new RegExp('\s*Version\/([0-9\.]+)\s*')).exec(UA)[0].trim().split('/')[1];
+    } else if (isChrome) {
+      Version = (new RegExp('\s*Chrome\/([0-9\.]+)\s*')).exec(UA)[0].trim().split('/')[1];
+    } else if (isFirefox) {
+      Version = (new RegExp('\s*Firefox\/([0-9\.]+)\s*')).exec(UA)[0].trim().split('/')[1];
+    } else if (isExplorer) {
+      Version = /\s*MSIE\s+([0-9\.]+)\s*/.exec(UA)[1];
+    } else if (isOpera && isWebkit) {
+      Version = (new RegExp('\s*OPR\/([0-9\.]+)\s*')).exec(UA)[0].trim().split('/')[1];
+    }
+    Version = Version.split('.');
+    Version = parseFloat(Version.shift() + '.' + Version.join(''));
+    return {
+      agent: isSafari ? 'safari' : (isChrome ? 'chrome' : (isFirefox ? 'firefox' : (isExplorer ? 'ie' : (isOpera ? 'opera' : 'unknown')))),
+      ver: Version,
+      os: isMobile ? (isAndroid ? 'android' : (isApple ? 'ios' : 'mobile')) : (isWindows ? 'windows' : (isMacintosh ? 'macintosh' : 'unknown'))
+    };
+  })();
+
   window.hex = hex;
 
   window.def = hex.add;
+
+  window.glob = function(fx) {
+    return $(document).ready(fx);
+  };
+
+  window.declare = function(code) {
+    return code();
+  };
+
+  window.run = function(name, period) {
+    return setInterval((function() {
+      return hex.call(name);
+    }), period);
+  };
+
+  glob(function() {
+    $('html').attr('agent', hex.env.agent);
+    $('html').attr('version', hex.env.ver);
+    return $('html').attr('os', hex.env.os);
+  });
+
+  def('define', function(args) {
+    var key, value;
+    key = args[0];
+    value = args[1];
+    return window['_' + key] = value;
+  });
+
+  def('switch', function(args) {
+    var item;
+    item = args[0];
+    return hex.set(item, !(hex.get(item)));
+  });
 
   def('type', function(args) {
     var object, target;
