85de9a13e62e161eb56dd98b8fe054746837914e
root <root@arcanelux.vps.phps.kr>
2014-06-05 02:03:18 +0900

    wsgi수정

---
+++ webroot/wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import site
+
+#site.addsitedir('/home/pahkey/virtualenv/codejob/lib/python2.7/site-packages')
+site.addsitedir('/usr/local/lib/python2.7/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tinicube.settings'
+
+sys.path.insert(0, '/srv/www/tinicube/webroot')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
