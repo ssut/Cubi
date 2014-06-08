932ec59adecb35dd84f5b0d7d1b6f294b7087d83
ssut <ssut@ssut.me>
2014-06-08 20:26:04 +0900

    회원탈퇴 구현

--- webroot/member/urls.py
+++ webroot/member/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('member.views',
     url(r'^connect/fb/callback$', 'connect_facebook_callback', name='connect_facebook_callback'),
     url(r'^connect/fb/destroy$', 'disconnect_facebook', name='disconnect_facebook'),
     url(r'^signin/fb$', 'signin_with_facebook', name='signin_with_facebook'),
-    url(r'^signin/fb/callback$', 'signin_with_facebook_callback', name='signin_with_facebook_callback')
+    url(r'^signin/fb/callback$', 'signin_with_facebook_callback', name='signin_with_facebook_callback'),
+
+    url(r'delete/$', 'delete_account', name='delete_account')
 )
