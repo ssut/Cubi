782b3bb3ab46cc047c6b2c5ee8badac6b105bcbe
ssut <ssut@ssut.me>
2014-06-12 00:39:13 +0900

    structure app pep8 적용

--- webroot/structure/views.py
+++ webroot/structure/views.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response, redirect, HttpResponse
 from django.template import RequestContext
 
@@ -8,7 +8,6 @@ try:
     User = get_user_model()
 except ImportError:
     from django.contrib.auth.models import User
-
 
 from work.models import *
 
@@ -22,7 +21,6 @@ def index(request):
 def about(request):
     return render_to_response('about.html')
 
-
 def reg_author(request):
     return render_to_response('reg_author.html')
 
@@ -34,11 +32,13 @@ def work_list(request):
     d = {
         'works': works,
     }
-    return render_to_response('work/work_list.html', d, RequestContext(request))
+    return render_to_response('work/work_list.html', d,
+                              RequestContext(request))
 
 def author_list(request):
     authors = User.objects.filter(type='2')
     d = {
         'authors': authors,
     }
-    return render_to_response('index/author_list.html', d, RequestContext(request))
+    return render_to_response('index/author_list.html', d,
+                              RequestContext(request))
