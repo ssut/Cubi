52a827bad417260b773a8cad0362134ac245f46d
ssut <ssut@ssut.me>
2014-06-20 01:54:24 +0900

    Fix paths

--- webroot/wsgi.py
+++ webroot/wsgi.py
@@ -6,7 +6,7 @@ site.addsitedir('/usr/local/lib/python2.7/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'tinicube.settings'
 os.environ['DJANGO_PRODUCTION'] = '1'
 
-sys.path.insert(0, '/srv/www/tinicube/webroot')
+sys.path.insert(0, '/var/www/tinicube/webroot')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
